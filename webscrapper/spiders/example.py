472d3de02fb0896b328f74f9be7bf8b561f43258
Milan Sawicki <milan.sawicki@student.put.edu.pl>
2017-04-13 03:30:15 +0200

    pagination and crawl through tv series

--- webscrapper/spiders/example.py
+++ webscrapper/spiders/example.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.loader import ItemLoader
 from webscrapper.items import MovieItem
-from scrapy.contrib.loader.processor import TakeFirst
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 class QuotesSpider(scrapy.Spider):
     name = "topFilms"
@@ -10,17 +10,27 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://www.filmweb.pl/search/serial?q=&type=&startYear=&endYear=&countryIds=&genreIds=&startRate=&endRate=&startCount=1000&endCount=&sort=COUNT&sortAscending=false&c=portal&page=1',
+            'http://www.filmweb.pl/search/serial',
         ]
+        rules = (Rule(LinkExtractor(allow=(), restrict_xpaths=('//a[@class="button next"]',)), callback="parse_page",
+                      follow=True),)
+
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse_page(self, response):
-        index =1;
-        for position in response.css('.filmPreview '):
+    def parse(self, response):
+
+        for position in response.css('.filmContent '):
             item = MovieItem()
             item['title'] = position.css('.filmSubtitle::text').extract_first()
             item['title_pl'] = position.css('.filmTitle::text').extract_first()
             item['year'] = position.css('.infoYear::text').extract_first()
-            item['plot'] = position.css('.filmPlot::text').extract_first()
+            # item['plot'] = position.css('.filmPlot::text').extract_first()
             yield item
+
+        next_page = response.xpath('//*[@class="nextLink"]//@href').extract()
+        if next_page:
+            next_href = next_page[0]
+            next_page_url = 'http://www.filmweb.pl/search/serial' + next_href
+            request = scrapy.Request(url=next_page_url)
+            yield request
